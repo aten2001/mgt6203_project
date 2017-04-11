9f82e6ccf07ce07b10d490ade73e85ea1319c696
cshanes <cshanes@umich.edu>
2017-04-11 11:29:49 -0400

    work income data

--- regression_analysis.R
+++ regression_analysis.R
@@ -37,3 +37,25 @@ ggplot(data=data, aes(factor(female_head_race), response)) +
   geom_boxplot() + 
   xlab("Female head race") + 
   ylab("Units purchased w coupon/units purchased")
+
+
+# appliance data analysis
+appliance_data = read.csv("~/gatech/mgt6203/project/appliance_data.csv",header=TRUE,sep=",",stringsAsFactors=FALSE)
+
+appliance_data$num_large_appliances = as.numeric(appliance_data$num_large_appliances)
+appliance_data$num_small_appliances = as.numeric(appliance_data$num_small_appliances)
+
+# create the response variable
+appliance_data$total_units_purchased = appliance_data$total_units_purchased / 100
+appliance_data$response = appliance_data$total_units_purchased_on_store_coup / appliance_data$total_units_purchased
+
+model <- lm(formula = response~rim_market + num_large_appliances + num_small_appliances, data=appliance_data)
+summary(model)
+
+# scatterplot
+ggplot(appliance_data, aes(x=num_large_appliances, y=response)) + geom_point()
+
+# boxplots
+ggplot(appliance_data, aes(x=factor(num_large_appliances), y=response)) + geom_boxplot()
+ggplot(appliance_data, aes(x=factor(num_small_appliances), y=response)) + geom_boxplot()
+
