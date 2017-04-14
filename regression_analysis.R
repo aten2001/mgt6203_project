73858fa78442388d15d5286e2f28b3489734df3f
cshanes <cshanes@umich.edu>
2017-04-14 19:05:23 -0400

    new regression analysis

--- regression_analysis.R
+++ regression_analysis.R
@@ -1,17 +1,42 @@
-data = read.csv("~/gatech/mgt6203/project/new_data.csv",header=TRUE,sep=",",stringsAsFactors=FALSE)
+library(lars)
+library(glmnet)
+library(plm)
 
-data$total_units_purchased = data$total_units_purchased / 100
-data$response = data$total_units_purchased_on_store_coup / data$total_units_purchased
+data = read.csv("~/gatech/mgt6203/project/mgt6203_project/consumer_analysis.csv",header=TRUE,sep=",",stringsAsFactors=FALSE, row.names=NULL)
 
-data$male_head_race = as.factor(data$male_head_race)
-data$female_head_race = as.factor(data$female_head_race)
+data$proportion_purchased_store_coup = data$total_units_purchased_on_store_coup / data$total_units_purchased
+data$brand = as.factor(data$brand)
+data$rim_market = as.factor(data$rim_market)
+data$household_income = as.factor(data$household_income)
 
-model <- lm(formula = response~rim_market + household_income + num_members_in_household + male_head_race + female_head_race, data=data)
+model <- lm(formula = proportion_purchased_store_coup ~ Q2 + Q3 + Q4 + rim_market +  
+                      num_small_appliances + num_members_in_household + household_income + 
+                      primary_head_avg_work_hours, data=data)
 summary(model)
+
+
+data$proportion_purchased_mfr_coup = data$total_units_purchased_on_mfr_coup / data$total_units_purchased
+model <- lm(formula = proportion_purchased_mfr_coup ~ Q2 + Q3 + Q4 + rim_market + num_large_appliances + 
+              num_small_appliances + num_pets + num_members_in_household + household_income + 
+              brand + primary_head_avg_work_hours, data=data)
+summary(model)
+
+# https://stats.stackexchange.com/questions/66973/difference-between-fixed-effects-models-in-r-plm-and-stata-xtreg
+
+fe_model <- plm(proportion_purchased_store_coup ~ Q2 + Q3 + Q4 + rim_market + num_large_appliances + 
+                  num_small_appliances + num_pets + num_members_in_household + household_income + 
+                  brand + primary_head_avg_work_hours, 
+               data   = data, 
+               method = "within", #fixed effects model
+               effect = "time", #does the gamma_i and delta_t parts (I think)
+               index  = c("brand")
+              )
+
+summary(fe_model)
 
 library(ggplot2)
 
-ggplot(data=data, aes(factor(rim_market), response)) +
+ggplot(data=data, aes(factor(rim_market), proportion_purchased_store_coup)) +
   geom_boxplot() + 
   xlab("ERIM Market") + 
   ylab("Units purchased w coupon/units purchased")
@@ -38,24 +63,4 @@ ggplot(data=data, aes(factor(female_head_race), response)) +
   xlab("Female head race") + 
   ylab("Units purchased w coupon/units purchased")
 
-
-# appliance data analysis
-appliance_data = read.csv("~/gatech/mgt6203/project/appliance_data.csv",header=TRUE,sep=",",stringsAsFactors=FALSE)
-
-appliance_data$num_large_appliances = as.numeric(appliance_data$num_large_appliances)
-appliance_data$num_small_appliances = as.numeric(appliance_data$num_small_appliances)
-
-# create the response variable
-appliance_data$total_units_purchased = appliance_data$total_units_purchased / 100
-appliance_data$response = appliance_data$total_units_purchased_on_store_coup / appliance_data$total_units_purchased
-
-model <- lm(formula = response~rim_market + num_large_appliances + num_small_appliances, data=appliance_data)
-summary(model)
-
-# scatterplot
-ggplot(appliance_data, aes(x=num_large_appliances, y=response)) + geom_point()
-
-# boxplots
-ggplot(appliance_data, aes(x=factor(num_large_appliances), y=response)) + geom_boxplot()
-ggplot(appliance_data, aes(x=factor(num_small_appliances), y=response)) + geom_boxplot()
 
